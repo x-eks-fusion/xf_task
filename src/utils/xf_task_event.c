d9bb83060f5b85cbfa867e4d6e57766877bcdb1d
cangyu <sky.kirto@qq.com>
2025-04-07 10:14:09 +0800

    🦄 refactor:  增加防止重复注册的检测

--- src/utils/xf_task_event.c
+++ src/utils/xf_task_event.c
@@ -39,16 +39,23 @@ xf_err_t xf_task_event_reg(xf_task_event_t *event, xf_task_t task, uint32_t even
                            xf_task_event_mode_t mode)
 {
     XF_ASSERT(event, XF_ERR_INVALID_ARG, TAG, "event must not be NULL");
-    xf_task_event_sub_t *sub = (xf_task_event_sub_t *)malloc(sizeof(xf_task_event_sub_t));
+    xf_task_event_sub_t *sub = NULL;
+    xf_list_for_each_entry(sub, &event->event_list, xf_task_event_sub_t, node) {
+        if (sub->task == task) {
+            XF_LOGE(TAG, "The task has been registered!");
+            return XF_ERR_BUSY;
+        }
+    }
+    sub = (xf_task_event_sub_t *)xf_malloc(sizeof(xf_task_event_sub_t));
     if (sub == NULL) {
         XF_LOGE(TAG, "memory alloc failed!");
-        return NULL;
+        return XF_ERR_NO_MEM;
     }
     sub->task = task;
     sub->event = event_value;
     sub->mode = mode;
     xf_list_init(&sub->node);
-    xf_list_add(&sub->node, &event->task_list);
+    xf_list_add(&sub->node, &event->event_list);
     return XF_OK;
 }
 
@@ -56,7 +63,7 @@ xf_err_t xf_task_event_unreg(xf_task_event_t *event, xf_task_t task)
 {
     XF_ASSERT(event, XF_ERR_INVALID_ARG, TAG, "event must not be NULL");
     xf_task_event_sub_t *sub = NULL, *tmp = NULL;
-    xf_list_for_each_entry_safe(sub, tmp, &event->task_list, xf_task_event_sub_t, node) {
+    xf_list_for_each_entry_safe(sub, tmp, &event->event_list, xf_task_event_sub_t, node) {
         if (sub->task == task) {
             xf_list_del_init(&sub->node);
             xf_free(sub);
@@ -72,7 +79,7 @@ xf_err_t xf_task_event_sent(xf_task_event_t *event, uint32_t event_value)
     XF_ASSERT(event, XF_ERR_INVALID_ARG, TAG, "event must not be NULL");
 
     xf_task_event_sub_t *sub = NULL;
-    xf_list_for_each_entry(sub, &event->task_list, xf_task_event_sub_t, node) {
+    xf_list_for_each_entry(sub, &event->event_list, xf_task_event_sub_t, node) {
         switch (sub->mode) {
         case XF_TASK_EVENT_OR:
             if ((sub->event | event_value) == sub->event) {
